6416af30d3bd7da92aba8789f7db0043fbddc38a
pavelsorokin <psorokin59@gmail.com>
2024-11-27 18:02:39 +0300

    init

---
+++ src/main/java/dev/sorokin/gitlabcicdytexample/GitlabCicdYtExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.sorokin.gitlabcicdytexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCicdYtExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabCicdYtExampleApplication.class, args);
+    }
+
+}
